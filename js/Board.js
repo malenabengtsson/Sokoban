a7c63b40977ba7c03ea0eeefb007f80e93217193
Malena Bengtsson <malena94bengtsson@gmail.com>
2019-11-25 15:04:28 +0100

    Testing grid

--- js/Board.js
+++ js/Board.js
@@ -1,3 +1,13 @@
 export default{
-    
+
+    template:`
+    <div class="container">
+    <div class="one">One</div>
+    <div class="two">Two</div>
+    <div class="three">three</div>
+    <div class="four">four</div>
+    <div class="five">five</div>
+    <div class="six">six</div>
+    </div>
+    `
 }
