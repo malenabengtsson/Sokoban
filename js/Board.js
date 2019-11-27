20ce02a47b830135ee674060d0a717a249fc41f1
Malena Bengtsson <malena94bengtsson@gmail.com>
2019-11-27 08:37:05 +0100

    testing array

--- js/Board.js
+++ js/Board.js
@@ -1,9 +1,13 @@
+import Block from './Block.js'
 export default{
+    components:{
+  
+    },
     template:`
     <div id="board">
         <div class="x"v-for="test in x">
-            <div class="y" v-for="test in y">
-                <img src="https://i.ytimg.com/vi/TVKOjnPJtNQ/maxresdefault.jpg" width=30 height=30>
+            <div class="y" v-for="test in x">
+                <img src="block.picture" width=30 height=30>
             </div>
         </div>
         
@@ -17,7 +21,13 @@ export default{
         }
     },
     methods:{
-        
+        addToArray: function(){
+            for(let i = 0; i<21; i++){
+                let block = new Block(i, i, i, "https://i.ytimg.com/vi/TVKOjnPJtNQ/maxresdefault.jpg")
+                this.x.push(block)
+                this.y.push(block)
+            }
+        }
     }
 
     /*template:`
